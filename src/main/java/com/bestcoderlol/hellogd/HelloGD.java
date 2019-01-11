dc9fd28895e09d24ebd9b089925c5a5f75dba822
GD <gd1902yt@gmail.com>
2019-01-11 20:58:03 +0100

    test1

--- src/main/java/com/bestcoderlol/hellogd/HelloGD.java
+++ src/main/java/com/bestcoderlol/hellogd/HelloGD.java
@@ -1,7 +1,19 @@
 package com.bestcoderlol.hellogd;
 
+import java.util.Scanner;
+
 public class HelloGD {
     public static void main(String[] args) {
         System.out.println("Hello I am GD");
+        Scanner sc = new Scanner(System.in);
+        String tx = sc.next();
+        System.out.println("Deine Antwort war : "+tx);
+        String tx1 = sc.next();
+        System.out.println("Du bist "+tx1+" jahre alt");
+        System.out.println("was machst du eig. gerade");
+        String tx2 = sc.next();
+        System.out.println("ok cool");
+
+
     }
 }
